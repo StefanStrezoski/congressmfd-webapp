4c0f8ebb5a2fd5054fd815ddda3637a4f52d05a0
StefanStrezoski <stefan.strezo98@gmail.com>
2026-03-01 01:22:49 +0100

    Remove small card from program page

--- src/views/ProgramPage.vue
+++ src/views/ProgramPage.vue
@@ -1,7 +1,6 @@
 <script setup>
 import BaseContainer from "@/components/BaseContainer.vue";
 import BaseCard from "@/components/BaseCard.vue";
-import SmallCard from "@/components/SmallCard.vue";
 import BaseParagraph from "@/components/BaseParagraph.vue";
 import homeimg from "@/assets/dates.png";
 </script>
